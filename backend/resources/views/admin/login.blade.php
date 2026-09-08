012dd00c83c3575508bb3a58befa0018cde5009f
nguyenxuanmin <nguyenxuanmin1998@gmail.com>
2026-09-08 10:24:12 +0700

    amend

--- backend/resources/views/admin/login.blade.php
+++ backend/resources/views/admin/login.blade.php
@@ -7,7 +7,7 @@
         @if (!empty($company->favicon))
             <link rel="icon" href="{{asset('storage/company/favicon/'.$company->favicon)}}" type="favicon">
         @endif
-        <title>Đăng nhập</title>
+        <title>Login</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI=" crossorigin="anonymous"/>
         <link rel="stylesheet" href="{{ asset('css/admin.css') }}"/>
